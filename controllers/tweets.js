be77941ed3cae4e252ce2b35b3a09434600b6605
David Garber & Kevin Chen <student@epicodus.com>
2015-06-09 13:46:11 -0700

    Add a time stamp to an individual tweet.

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -3,7 +3,7 @@ Twitter.TweetsController = Ember.ArrayController.extend({
   sortProperties: ['id'],
   actions: {
     tweet: function() {
-      var newTweet = {id: (tweets.length + 1), tweet: this.get('new-tweet')};
+      var newTweet = {id: (tweets.length + 1), tweet: this.get('new-tweet'), time: (Date.now())};
       tweets.addObject(newTweet);
     }
   }
